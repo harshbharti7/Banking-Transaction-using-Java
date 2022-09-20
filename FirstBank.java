5799dca6f7a7072d57edde85249976bc60af2fa6
harshbharti7 <66279783+harshbharti7@users.noreply.github.com>
2022-09-20 20:57:27 +0530

    Add files via upload

---
+++ FirstBank.java
@@ -0,0 +1,16 @@
+//Java Banking Application Project
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account tim = new Account("Harsh Crickstar", "A00001");
+		
+		
+		Account beyonce = new Account("HB", "B00002");
+		beyonce.showMenu();
+		
+		
+	}
+
+}
